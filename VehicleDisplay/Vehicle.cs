e570615235a0db908587d4f1bf3cfbcdd1f05bb7
Alexandra Weiß <baronesse.poubelle@gmail.com>
2020-04-28 11:09:27 +0200

    Switched application to Dotnet Core and MVVMLight

---
+++ VehicleDisplay/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace VehicleDisplay
+{
+    abstract class Vehicle
+    {
+        #region Vehicle data
+        public string Name { get; private set; }
+
+        public Guid Id { get; private set; }
+
+        public string Model { get; private set; }
+
+        public enum VehicleType
+        {
+            CAR, TRUCK, MOTORCYCLE
+        }
+
+        public VehicleType Type { get; private set; }
+
+        public Point Position { get; set; }
+
+        public float Speed { get; set; }
+
+        #endregion
+    }
+}
