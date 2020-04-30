bed93b4b681a857d25465bdac9c80e8c42d5545d
Alexandra Weiß <baronesse.poubelle@gmail.com>
2020-04-30 15:21:20 +0200

    Project set up to use MVVMLight with a MainViewModel and a ViewModelLocator

---
+++ VehicleDisplay/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace VehicleDisplay.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                WindowTitle = "Vehicle Display (Design Mode)";
+            }
+            else
+            {
+                WindowTitle = "VehicleDisplay";
+            }
+        }
+
+
+        public string WindowTitle { get; private set; }
+    }
+}
